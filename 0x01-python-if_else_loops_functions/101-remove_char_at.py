c3e8c3bcf7433c24bdcdfcacf29bfa9ca6138a0b
carolyneMuthe <mutheuc08@gmail.com>
2024-09-26 05:46:38 -0700

    Remove at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+def remove_char_at(str, n):
+    # If n is out of range, return the string as is
+    if n < 0 or n >= len(str):
+        return str
+    
+    # Create a new string by excluding the character at position n
+    return str[:n] + str[n+1:]
