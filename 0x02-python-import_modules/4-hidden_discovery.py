8e3bdfd363707b382e068f38978a72b28ff624aa
carolyneMuthe <mutheuc08@gmail.com>
2024-09-30 14:54:32 -0700

    printing names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,7 +5,8 @@ import sys
 if __name__ == "__main__":
     # Load the compiled module 'hidden_4.pyc'
     module_name = "hidden_4"
-    spec = importlib.util.spec_from_file_location(module_name, "./hidden_4.pyc")
+    spec = importlib.util.spec_from_file_location(module_name,
+            "./hidden_4.pyc")
     hidden_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(hidden_module)
 
