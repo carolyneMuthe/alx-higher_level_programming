f4e7f1abc45970e160b5ffb0a0fb8d1335ef2788
carolyneMuthe <mutheuc08@gmail.com>
2024-09-30 14:40:33 -0700

    a little fix again!

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ import sys
 
 if __name__ == "__main__":
     argv = sys.argv[1:]  # Exclude the script name
-    result = sum(int(arg) for arg in arg)
+    result = sum(int(arg) for arg in argv)
     print(result)
