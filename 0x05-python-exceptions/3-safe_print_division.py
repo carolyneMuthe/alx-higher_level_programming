5d86614783d19b116058b40620a34d188b796f6c
carolyneMuthe <mutheuc08@gmail.com>
2024-10-08 14:00:55 -0700

    Divide a list

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -9,5 +9,5 @@ def safe_print_division(a, b):
     finally:
         # Print the result or None if an exception occurred
         print("Inside result: {}".format(result))
-    
+
     return result  # Return the result of the division
