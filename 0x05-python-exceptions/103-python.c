637a23ca608e8489fa60651f45c214b1b27950f0
carolyneMuthe <mutheuc08@gmail.com>
2024-10-08 14:45:02 -0700

    CPython #2: PyFloatObject

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,39 +1,59 @@
 #include <Python.h>
+#include <stdio.h>
 
-/* Forward declaration of print_python_bytes */
 void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
 
 void print_python_list(PyObject *p) {
+    Py_ssize_t size = 0; // Initialize size
+    PyObject *item;
+
     // Check if p is a list
     if (!PyList_Check(p)) {
         fprintf(stderr, "Provided object is not a list\n");
         return;
     }
 
-    Py_ssize_t size = PyList_Size(p);
-    printf("[");
+    // Manually calculate the size of the list
+    while (PyList_Check(p) && (item = PyList_GetItem(p, size)) != NULL) {
+        size++;
+    }
+
+    printf("[\n");
     for (Py_ssize_t i = 0; i < size; i++) {
-        PyObject *item = PyList_GetItem(p, i);
-        print_python_bytes(item);  // Call to the function
-        if (i < size - 1) {
-            printf(", ");
+        item = PyList_GetItem(p, i);
+        printf("  ");
+        if (PyBytes_Check(item)) {
+            print_python_bytes(item);
+        } else if (PyFloat_Check(item)) {
+            print_python_float(item);
+        } else {
+            printf("<not a bytes or float object>\n");
         }
     }
     printf("]\n");
 }
 
 void print_python_bytes(PyObject *p) {
-    // Check if p is a bytes object
     if (!PyBytes_Check(p)) {
         fprintf(stderr, "Provided object is not bytes\n");
         return;
     }
 
-    // Print byte object details
     Py_ssize_t size = PyBytes_Size(p);
     printf("Bytes object of size %zd: ", size);
     for (Py_ssize_t i = 0; i < size; i++) {
-        printf("%02x ", (unsigned char) PyBytes_AsString(p)[i]);
+        printf("%02x ", (unsigned char)PyBytes_AsString(p)[i]);
     }
     printf("\n");
+}
+
+void print_python_float(PyObject *p) {
+    if (!PyFloat_Check(p)) {
+        fprintf(stderr, "Provided object is not a float\n");
+        return;
+    }
+
+    double value = PyFloat_AsDouble(p);
+    printf("Float value: %f\n", value);
 }
