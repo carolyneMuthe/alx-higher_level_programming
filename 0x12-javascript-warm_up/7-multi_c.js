7fc10c147709e4a852ee5e1ec7a7989cef735d14
carolyneMuthe <mutheuc08@gmail.com>
2024-12-16 09:44:09 -0800

    correction

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
+
 const x = parseInt(process.argv[2]);
 
 if (isNaN(x) || x <= 0) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  let i = 0;
+  while (i < x) {
     console.log('C is fun');
+    i++;
   }
 }
